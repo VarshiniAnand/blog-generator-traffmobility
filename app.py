d0b974aec39855c63e180622d77ec90e56ac1203
VarshiniAnand <37431249+VarshiniAnand@users.noreply.github.com>
2025-06-09 22:20:32 -0400

    Updated port to get from env or render default port

--- app.py
+++ app.py
@@ -83,4 +83,5 @@ def generate_blog():
 
 # Required for Render
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
